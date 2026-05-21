cb74c70603557edc62d053a1bdefde1cf616c09d
Evgeniya Karpunina <janekarpunina@gmail.com>
2026-05-21 11:01:58 +0400

    Added dockerfiles, docker compose

--- storefront-app/src/main/java/ru/yandex/practicum/mymarket/config/PaymentClientConfig.java
+++ storefront-app/src/main/java/ru/yandex/practicum/mymarket/config/PaymentClientConfig.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.mymarket.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import ru.yandex.practicum.mymarket.ApiClient;
@@ -7,9 +8,13 @@ import ru.yandex.practicum.mymarket.api.PaymentApi;
 
 @Configuration
 public class PaymentClientConfig {
+
+    @Value("${app.payment-service.url:http://localhost:8081}")
+    private String paymentServiceUrl;
+
     @Bean
     public ApiClient paymentApiClient() {
-        return new ApiClient().setBasePath("http://localhost:8081");
+        return new ApiClient().setBasePath(paymentServiceUrl);
     }
 
     @Bean
